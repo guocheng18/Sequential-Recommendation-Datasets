daef0c1cf5692b80d6bd3dce3407477a859126ec
guocheng <guocheng672@gmail.com>
2019-11-27 23:35:59 +0800

    bug fix

--- tests/test_download.py
+++ tests/test_download.py
@@ -6,5 +6,5 @@ def test_download_all_datasets():
     _download("Amazon-Apps")
     _download("FourSquare-NYC")
     for d in __datasets__:
-        if not d.startswith("Amazon") and not d.startswith("FourSquare")
+        if not d.startswith("Amazon") and not d.startswith("FourSquare"):
             _download(d)
