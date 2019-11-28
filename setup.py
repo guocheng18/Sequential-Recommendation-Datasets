edad426efeb7248b55af93932571440246017ce9
guocheng <guocheng672@gmail.com>
2019-11-28 11:52:06 +0800

    fix test bug

--- setup.py
+++ setup.py
@@ -5,6 +5,8 @@ VERSION = "0.0.7"
 with open("README.md", "r") as f:
     long_description = f.read()
 
+with open("requirements.txt", "r") as f:
+    install_requires = f.read().splitlines()
 
 setup(
     name="srdatasets",
@@ -17,12 +19,7 @@ setup(
     url="https://github.com/guocheng2018/sequential-recommendation-datasets",
     packages=find_packages(),
     python_requires=">=3.6",
-    install_requires=[
-        "pandas>=0.25.0",
-        "tqdm>=4.33.0",
-        "tabulate>=0.8.5",
-        "numpy>=1.16.4",
-    ],
+    install_requires=install_requires,
     classifiers=[
         "Development Status :: 4 - Beta",
         "Intended Audience :: Developers",
